f04cdb2b8ae75d279b3cfde1306a70d7311a5fbf
Wenhao Wu <whwu.ucas@gmail.com>
2023-04-18 22:46:16 +0800

    update grad_checkpoint to save memory

--- clip/model.py
+++ clip/model.py
@@ -220,7 +220,7 @@ class ResidualAttentionBlock(nn.Module):
         self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device) if self.attn_mask is not None else None
         return self.attn(x, x, x, need_weights=False, attn_mask=self.attn_mask)[0]
 
-    def forward(self, x: torch.Tensor, with_cp=False):
+    def forward(self, x: torch.Tensor, with_cp=True):
         x = self.control_point(x)
         if with_cp:
             attn_out = checkpoint.checkpoint(self.attention, self.ln_1(x))
@@ -247,9 +247,15 @@ class Transformer(nn.Module):
         self.layers = layers
         
         self.resblocks = nn.Sequential(*[ResidualAttentionBlock(width, heads, attn_mask, dropout=dropout[i]) for i in range(layers)])
+        self.grad_checkpointing = True
 
     def forward(self, x: torch.Tensor):
-        return self.resblocks(x)
+        for r in self.resblocks:
+            if self.grad_checkpointing and not torch.jit.is_scripting():
+                x = checkpoint.checkpoint(r, x)
+            else:
+                x = r(x)
+        return x
 
 
 class VisualTransformer(nn.Module):
